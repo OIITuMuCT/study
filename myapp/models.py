402845908523cdae1d5f41ff0f04b61b40c50ada
OIITuMuCT <vataman@live.ru>
2024-10-04 23:39:14 +0300

    create model Review and add migrations

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
 class Publisher(models.Model):
@@ -7,13 +8,14 @@ class Publisher(models.Model):
         max_length=50,
         help_text="The name of the Publisher.")
     website = models.URLField(
-        help_text="The Publisher's website"
-    )
+        help_text="The Publisher's website")
     email = models.EmailField(
-        help_text="The Publisher's email address."
-    )
+        help_text="The Publisher's email address.")
+    
+    def __str__(self):
+        return self.name
 
-class Book(modles.Model):
+class Book(models.Model):
     """ A published book. """
     title = models.CharField(
         max_length=70, help_text='The title of the book.')
@@ -27,6 +29,10 @@ class Book(modles.Model):
         Publisher, on_delete=models.CASCADE)
     contributors = models.ManyToManyField(
         'Contributor', through="BookContributor")
+    
+    def __str__(self):
+        return self.title
+    
 
 class Contributor(models.Model):
     """A contributor to a Book, e.g. author, editor, co-author. """
@@ -35,8 +41,10 @@ class Contributor(models.Model):
     last_names = models.CharField(
         max_length=50, help_text="The contributor's last name of names")
     email = models.EmailField(
-        help_text="The contact email for the contributor."
-    )
+        help_text="The contact email for the contributor.")
+    
+    def __str__(self):
+        return self.first_names
 
 class BookContributor(models.Model):
     class ContributionRole(models.TextChoices):
@@ -52,3 +60,22 @@ class BookContributor(models.Model):
         verbose_name="The role this contributor had in the \
             book.", choices=ContributionRole.choices, max_length=20
     )
+
+class Review(models.Model):
+    content = models.TextField(
+        help_text="The Review text.")
+    rating = models.IntegerField(
+        help_text="The rating the reviewer has given.")
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        help_text="The date and time the review was \
+            created.")
+    date_edited = models.DateTimeField(
+        null=True, help_text="The date and time the review \
+            was last edited."
+    )
+    creator = models.ForeignKey(
+        auth.get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE,
+        help_text="The Book that this review is for.")
