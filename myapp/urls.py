bd60e69db1876e188149d4e4aa27d886feab54a5
OIITuMuCT <vataman@live.ru>
2024-10-17 19:47:59 +0300

    fix path  reviwe_edit

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,12 +5,16 @@ from . import views
 urlpatterns = [
     path("", views.welcome_view, name="welcome_view"),
     path("books/", views.book_list, name="book_list"),
-    path("books/<int:pk>/", views.book_detail, name='book_detail'),
+    path("books/<int:pk>/", views.book_detail, name="book_detail"),
     path("polls/", views.survey, name="polls"),
     path("home-page/", views.HomePage.as_view(), name="home_page"),
-    path("index/", views.index, name='index'),
+    path("index/", views.index, name="index"),
     path("publishers/<int:pk>", views.publisher_edit, name="publisher_edit"),
     path("publishers/new/", views.publisher_edit, name="publisher_create"),
     path("books/<int:book_pk>/reviews/new/", views.review_edit, name="review_create"),
-    path("books/<int:book_pk>/reviews/<int:review_pk/", views.review_edit, name="review_edit"),
+    path(
+        "books/<int:book_pk>/reviews/<int:review_pk>/",
+        views.review_edit,
+        name="review_edit",
+    ),
 ]
