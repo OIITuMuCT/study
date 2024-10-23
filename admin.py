45701a22e58bd6f4f2d466783d464c15c76a2600
OIITuMuCT <vataman@live.ru>
2024-10-23 17:09:07 +0300

    admin_profile non worked

--- admin.py
+++ admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
 
 
 class BookrAdminSite(admin.AdminSite):
     title_header = "Bookr Admin"
-    site_header = "Bookr administration"
+    site_header = "Bookr Administration Portal"
     index_title = "Bookr site admin"
--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -3,30 +3,24 @@ from django.contrib.auth.admin import User
 from django.template.response import TemplateResponse
 from django.urls import path
 
-
-# Register your models here.
 class BookrAdmin(admin.AdminSite):
-    site_header = "Bookr Administration"
-    site_title = "Bookr"
-    index_title = "Bookr site admin"
+    site_header = "Bookr Administration Portal"
     logout_template = 'admin/logout.html'
 
+    def profile_view(self, request):
+        request.current_app = self.name
+        context = {self.each_context(request)}
+        return TemplateResponse(request, "admin/admin_profile.html", context)
+        # context = super().each_context(request)
+        # context["username"] = request.user.username
+        # return context
+
+    def get_urls(self):
+        urls = super().get_urls()
+        url_patterns = [path("admin_profile", self.admin_view(self.profile_view))]
+        return url_patterns + urls
 
 
 admin_site = BookrAdmin(name='bookr_admin')
 admin_site.register(User)
 
-class SysAdminSite(admin.AdminSite):
-    def system_health_dashboard(self, request):
-        request.current_app = self.name
-        context = self.each_context(request)
-        # View function logic
-        return context
-
-    def get_urls(self):
-        base_urls = super().get_urls() # get the existing set of URLs
-        #Define our URL patterns for custom views
-        urlpatterns = [
-            path("health_dashboard/", self.system_health_dashboard)
-        ]
-        return base_urls + urlpatterns # Return the update mapping 
