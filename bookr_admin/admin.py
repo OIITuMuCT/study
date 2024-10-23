4ce435745c744df5f490eac75d19fdbc49c8a182
OIITuMuCT <vataman@live.ru>
2024-10-23 16:35:53 +0300

    SysAdminSite test class

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import User
+from django.template.response import TemplateResponse
+from django.urls import path
 
 
 # Register your models here.
@@ -10,6 +12,7 @@ class BookrAdmin(admin.AdminSite):
     logout_template = 'admin/logout.html'
 
 
+
 admin_site = BookrAdmin(name='bookr_admin')
 admin_site.register(User)
 
@@ -18,11 +21,12 @@ class SysAdminSite(admin.AdminSite):
         request.current_app = self.name
         context = self.each_context(request)
         # View function logic
+        return context
 
     def get_urls(self):
         base_urls = super().get_urls() # get the existing set of URLs
         #Define our URL patterns for custom views
         urlpatterns = [
-            patn("health_dashboard/", self.system_health_dashboard)
+            path("health_dashboard/", self.system_health_dashboard)
         ]
         return base_urls + urlpatterns # Return the update mapping 
