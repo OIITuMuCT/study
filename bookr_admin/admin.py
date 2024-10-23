a4edab56b4794e25228531ebb05db96c0ce6bdbd
OIITuMuCT <vataman@live.ru>
2024-10-23 16:29:50 +0300

    exercise 10.03

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -7,8 +7,22 @@ class BookrAdmin(admin.AdminSite):
     site_header = "Bookr Administration"
     site_title = "Bookr"
     index_title = "Bookr site admin"
+    logout_template = 'admin/logout.html'
 
 
 admin_site = BookrAdmin(name='bookr_admin')
 admin_site.register(User)
 
+class SysAdminSite(admin.AdminSite):
+    def system_health_dashboard(self, request):
+        request.current_app = self.name
+        context = self.each_context(request)
+        # View function logic
+
+    def get_urls(self):
+        base_urls = super().get_urls() # get the existing set of URLs
+        #Define our URL patterns for custom views
+        urlpatterns = [
+            patn("health_dashboard/", self.system_health_dashboard)
+        ]
+        return base_urls + urlpatterns # Return the update mapping 
--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-c!ed4dqj0ous$i*%zf&xxf*skpgkey6%$ld-pz6s^_k6w#eo7&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
