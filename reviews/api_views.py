7f94074d66443f4a8c6298a2669687cb3dd9d4ce
OIITuMuCT <vataman@live.ru>
2024-10-24 20:46:01 +0300

    api_views imports

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework.views import APIView
 from rest_framework.pagination import (
     LimitOffsetPagination)
 
