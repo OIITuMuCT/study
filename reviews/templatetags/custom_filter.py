e2796372577de9c8054d84cd2a14507b4303f0e4
OIITuMuCT <vataman@live.ru>
2024-10-24 16:49:16 +0300

    Install REST

--- reviews/templatetags/custom_filter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from django.template.defaultfilters import stringfilter
-
-register = template.Library()
-
-# @register.filter
-# @stringfilter
-# def generic_string_filter(value, arg):
-#     # Logic for string filter implementation
