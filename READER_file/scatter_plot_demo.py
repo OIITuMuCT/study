f2b68e22df073121eab470a239390dca9c8af140
OIITuMuCT <vataman@live.ru>
2024-10-26 21:56:44 +0300

    create generate_scatter_plot

--- READER_file/scatter_plot_demo.py
+++ READER_file/scatter_plot_demo.py
@@ -5,3 +5,10 @@ def generate_scatter_plot(x_axis, y_axis):
     figure = graphs.Figure()
     scatter = graphs.Scatter(x=x_axis, y=y_axis)
     return plot(figure, output_type='div')
+
+def generate_scatter_plot(x_axis, y_axis):
+    figure = graphs.Figure()
+    scatter = graphs.Scatter(x=x_axis, y=y_axis)
+    figure.add_trace(scatter)
+    return plot(figure, output_type='div')
+
