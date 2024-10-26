d7ced60159bdfa89afa1020d90432a20bba23a19
OIITuMuCT <vataman@live.ru>
2024-10-26 21:52:52 +0300

    create scatter_plot_demo

---
+++ READER_file/scatter_plot_demo.py
@@ -0,0 +1,7 @@
+from plotly.offline import plot
+
+def generate_scatter_plot(x_axis, y_axis):
+    """ Generate a scatter plot for the provided x and y-axis values. """
+    figure = graphs.Figure()
+    scatter = graphs.Scatter(x=x_axis, y=y_axis)
+    return plot(figure, output_type='div')
