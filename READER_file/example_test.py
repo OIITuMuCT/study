1470e226d758d36b490ae6870a2a8c3e5a0abc69
OIITuMuCT <vataman@live.ru>
2024-10-28 17:19:30 +0300

    add bookr_test app

---
+++ READER_file/example_test.py
@@ -0,0 +1,10 @@
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        # Do some initialization work
+    def test_method_a(self):
+        # code for testing method A 
+    def test_method_b(self):
+        # code for testing method B
+    def tearDown(self):
+        # perform cleanup
+        
