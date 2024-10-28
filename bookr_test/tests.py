3448c24eb64608f4adf041a9ae6c4dc583aafa52
OIITuMuCT <vataman@live.ru>
2024-10-28 18:19:47 +0300

    add TestLoggedInGreetingView

--- bookr_test/tests.py
+++ bookr_test/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from .models import Publisher
+
 
 
 # Create your tests here.
@@ -29,3 +31,20 @@ class TestGreetingView(TestCase):
         response = self.client.get('/test/greeting')
         self.assertEqual(response.status_code, 200)
 
+
+class TestLoggedInGreetingView(TestCase):
+    """ Test the greeting view for the authenticated users."""
+    def setUp(self):
+        test_user = User.objects.create_user(
+            username='testuser', password='test@#628password')
+        test_user.save()
+        self.client = Client()
+    def test_user_greeting_not_authenticated(self):
+        response = self.client.get('/test/greet_user')
+        self.assertEqual(response.status_code, 302)
+    def test_user_authenticated(self):
+        login = self.client.login(username='testuser', password='test@#628password')
+        response = self.client.get('/test/greet_user')
+        self.assertEqual(response.status_code, 200)
+        if response.status_code == 200:
+            print('Выполнено успешно!!!')
