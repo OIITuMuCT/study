b23fd43be34c45b8343b4f4557c3f5eca204dbfb
OIITuMuCT <vataman@live.ru>
2024-10-28 15:33:47 +0300

    fixbag generate_scatter_plot

--- READER_file/scatter_plot_demo.py
+++ READER_file/scatter_plot_demo.py
@@ -1,4 +1,5 @@
 from plotly.offline import plot
+import plotly.graph_objs as graphs
 
 
 def generate_scatter_plot(x_axis, y_axis):
@@ -8,9 +9,9 @@ def generate_scatter_plot(x_axis, y_axis):
     figure.add_trace(scatter)
     return plot(figure, output_type='div')
 
-def generete_html(plot_html):
+def generate_html(plot_html):
     """ Generate an HTML page for the provide plot. """
-    html_content = "<html><head><title>Plot Demo </title></head><body>{}</body>".format(plot_html)
+    html_content = "<html><head><title>Plot Demo </title></head><body>{}</body></html>".format(plot_html)
     try:
         with open('plot_demo.html', 'w') as plot_file:
             plot_file.write(html_content)
@@ -22,4 +23,4 @@ if __name__ == '__main__':
     x = [1,2,3,4,5]
     y = [3,8,7,9,2]
     plot_html = generate_scatter_plot(x, y)
-    generete_html(plot_html)
+    generate_html(plot_html)
