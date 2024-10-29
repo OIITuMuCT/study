34e5dd45dd1338e1efc64439cec35c443452fead
OIITuMuCT <vataman@live.ru>
2024-10-29 21:12:26 +0300

    end chapter15

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,21 @@
-from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from django import forms
+
 from .models import Publisher, Review, Book
+
+
+class InstanceForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+
+        if kwargs.get("instance"):
+            button_title = "Save"
+        else:
+            button_title = "Create"
+
+        self.helper.add_input(Submit("", button_title))
 
 
 class SearchForm(forms.Form):
@@ -9,20 +23,21 @@ class SearchForm(forms.Form):
     search_in = forms.ChoiceField(
         required=False, choices=(("title", "Title"), ("contributor", "Contributor"))
     )
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "get"
-        self.helper.add_input(Submit('', "Search"))
+        self.helper.add_input(Submit("", "Search"))
 
 
-class PublisherForm(forms.ModelForm):
+class PublisherForm(InstanceForm):
     class Meta:
         model = Publisher
         fields = "__all__"
 
 
-class ReviewForm(forms.ModelForm):
+class ReviewForm(InstanceForm):
     class Meta:
         model = Review
         exclude = ["date_edited", "book"]
@@ -30,7 +45,7 @@ class ReviewForm(forms.ModelForm):
     rating = forms.IntegerField(min_value=0, max_value=5)
 
 
-class BookMediaForm(forms.ModelForm):
+class BookMediaForm(InstanceForm):
     class Meta:
         model = Book
         fields = ["cover", "sample"]
